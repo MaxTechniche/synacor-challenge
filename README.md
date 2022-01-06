26647be5cf3130b97204dbf4693fce7639402900
Jacob <max.techniche@gmail.com>
2022-01-05 21:17:02 -0700

    completed each instruction code, but I still experience logic errors.

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # synacor-challenge
+
+```
+Welcome to the Synacor Challenge!
+Please record your progress by putting codes like
+this one into the challenge website: WaUzfpgoWdNL
+
+Executing self-test...
+
+==  ==
+
+- 
+
+There are  exits:
+- 
+- 
+```
--- program.py
+++ program.py
@@ -12,6 +12,30 @@ class VM:
         self.register = [0] * 8
         self.stack = []
         self.pos = 0
+        self.op = {
+            '0': self.op_0,
+            '1': self.op_1,
+            '2': self.op_2,
+            '3': self.op_3,
+            '4': self.op_4,
+            '5': self.op_5,
+            '6': self.op_6,
+            '7': self.op_7,
+            '8': self.op_8,
+            '9': self.op_9,
+            '10': self.op_10,
+            '11': self.op_11,
+            '12': self.op_12,
+            '13': self.op_13,
+            '14': self.op_14,
+            '15': self.op_15,
+            '16': self.op_16,
+            '17': self.op_17,
+            '18': self.op_18,
+            '19': self.op_19, 
+            '20': self.op_20,
+            '21': self.op_21
+        }
         
     def read(self, file_path: str=None) -> bool:
         if not file_path:
@@ -43,53 +67,13 @@ class VM:
         while True:
             num = self.memory[self.pos]
             
-            if num == 0:
-                self.op_0()
-            elif num == 1:
-                self.op_1()
-            elif num == 2:
-                self.op_2()
-            elif num == 3:
-                self.op_3()
-            elif num == 4:
-                self.op_4()
-            elif num == 5:
-                self.op_5()
-            elif num == 6:
-                self.op_6()
-            elif num == 7:
-                self.op_7()
-            elif num == 8:
-                self.op_8()
-            elif num == 9:
-                self.op_9()
-            elif num == 10:
-                self.op_10()
-            elif num == 11:
-                self.op_11()
-            elif num == 12:
-                self.op_12()
-            elif num == 13:
-                self.op_13()
-            elif num == 14:
-                self.op_14()
-            elif num == 15:
-                self.op_15()
-            elif num == 16:
-                self.op_16()
-            elif num == 17:
-                self.op_17()
-                
-            elif num == 19:
-                self.op_19()
-                
-            elif num == 21:
-                self.op_21()
-        
-            else:
+            if str(num) not in self.op:
                 print(num)
                 break
 
+            self.op[str(num)]()
+            
+            
     def op_0(self) -> None:
         sys.exit(0)
 
@@ -110,8 +94,11 @@ class VM:
         self.pos += 2
 
     def op_3(self) -> None:
-        a = self.memory[self.pos + 1] % 32768
-        self.register[a] = self.stack.pop()
+        a = self.memory[self.pos + 1]
+        if a >= 32768:
+            self.register[a % 32768] = self.stack.pop()
+        else:
+            self.memory[a] = self.stack.pop()
         self.pos += 2
 
     def op_4(self) -> None:
@@ -147,7 +134,7 @@ class VM:
         self.pos += 4
    
     def op_6(self) -> None:
-        a = self.memory[self.pos + 1] % 32768
+        a = self.memory[self.pos + 1]
         if a >= 32768:
             a = self.register[a % 32768]
         self.pos = a
@@ -303,29 +290,25 @@ class VM:
         b = self.memory[self.pos + 2]
         if b >= 32768:
             b = self.register[b % 32768]
-        a = self.memory[self.pos + 1]
-        if a >= 32768:
-            self.memory[self.register[a % 32768]] = b
-        else:
-            self.memory[self.memory[a]] = b
             
-        self.pos += 3
-        
-    
-    def op_17(self) -> None:
         a = self.memory[self.pos + 1]
         if a >= 32768:
             a = self.register[a % 32768]
+        self.memory[a] = b
+            
+        self.pos += 3
+         
+    def op_17(self) -> None:
+        self.stack.append(self.pos+2)
         
-        b = self.pos + 2
-        # if b >= 32768:
-        #     b = self.register[b % 32768]
-        self.stack.append(b)
-        
+        a = self.memory[self.pos + 1]
+        if a >= 32768:
+            a = self.register[a % 32768]
         self.pos = a
         
-    
-    
+    def op_18(self) -> None:
+        val = self.stack.pop()
+        self.pos = val
     
     def op_19(self) -> None:
         self.pos += 1
@@ -337,6 +320,14 @@ class VM:
             char = chr(self.memory[self.pos])
         print(char, end='')
         self.pos += 1
+        
+    def op_20(self) -> None:
+        a = self.memory[self.pos + 1]
+        if a >= 32768:
+            a = self.register[a % 32768]
+        self.memory[a] = ord(input())
+        self.pos += 2
+            
         
     def op_21(self) -> None:
         self.pos += 1
